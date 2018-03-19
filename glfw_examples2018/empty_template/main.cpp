a39ca2114912d616e1585785198153b0ed54494a
dsaf2007 <dsaf2007@naver.com>
2018-03-19 15:43:35 +0900

    number1

--- glfw_examples2018/empty_template/main.cpp
+++ glfw_examples2018/empty_template/main.cpp
@@ -8,8 +8,66 @@
 #include "math.h"
 
 const int width_window = 640;
-const int height_window = 480;
+const int height_window = 640;
 
+void drawFilledCircle(GLfloat x, GLfloat y, GLfloat radius) {
+	//속에 색이 찬 원을 그린다.
+	int i;
+	int triangleAmount = 200; //원을 그리는데 사용할 삼각형 갯수.
+
+							 
+	GLfloat twicePi = 2.0f * 3.141592f;//파이의 두배
+
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x, y); // 중심 좌표.
+	for (i = 0; i <= triangleAmount; i++) {
+		glVertex2f(
+			x + (radius * cos(i *  twicePi / triangleAmount)),
+			y + (radius * sin(i * twicePi / triangleAmount))
+		);
+	}
+	glEnd();
+}
+void drawFilledEllipse(GLfloat x, GLfloat y, GLfloat radius) {
+	//삼각형을 이용하여 타원을 그리는 함수
+	//원을 그리는 함수에서 x좌표가 찍히는 범위를 절반으로 하고 y좌표가 찍히는 범위를 90프로로 하였다.
+	int i;
+	int triangleAmount = 200; 
+
+							  
+	GLfloat twicePi = 2.0f * 3.141592f;
+
+	glBegin(GL_TRIANGLE_FAN);
+	glVertex2f(x, y); 
+	for (i = 0; i <= triangleAmount; i++) {
+		glVertex2f(
+			x + (radius / 2 * cos(i *  twicePi / triangleAmount)),
+			y + (radius*0.9 * sin(i * twicePi / triangleAmount))
+		);
+	}
+	glEnd();
+}
+
+void parabola() {
+	//입모양을 그리는 함수.
+	//포물선을 이용하였다.
+	GLfloat midY = -0.25;
+	GLfloat midX = 0.0;
+
+	GLfloat y = 0;
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glBegin(GL_POINTS);//포물선 식을 이용하여, 포물선 식 위에 점을 찍는다.
+	for (float a = 0; a < 0.025; a += 0.0005)
+	{
+		for (GLfloat x = -0.25; x <= 0.25; x += 0.0005) {
+			y = x * x;
+			glVertex2f(midX + x, midY + y + a);
+		}
+	}
+	glEnd();
+	glFlush();
+}
 int main(void)
 {
 	GLFWwindow *window = nullptr;
@@ -46,13 +104,18 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//TODO: draw here
-		glBegin(GL_LINES);
+		//glBegin(GL_LINES);
 
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, 0.0f);
-		glVertex3f(1.0f, 0.0f, 0.0f);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		drawFilledCircle(0, 0, 0.5);
+		glColor3f(1.0f, 1.0f, 0.0f);
+		drawFilledCircle(0, 0, 0.48);
+		glColor3f(0.0f, 0.0f, 0.0f);
+		drawFilledEllipse(0.15, 0.15, 0.1);
+		drawFilledEllipse(-0.15, 0.15, 0.1);
+		parabola();
 
-		glEnd();
+		//glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
